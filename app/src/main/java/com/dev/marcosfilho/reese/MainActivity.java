7a48f17f6bcf112cdfbcc7e141e5a587fb8f2d21
marcos <marcos.martins.filho@gmail.com>
2018-12-29 11:34:07 -0200

    new version

--- app/src/main/java/com/dev/marcosfilho/reese/MainActivity.java
+++ app/src/main/java/com/dev/marcosfilho/reese/MainActivity.java
@@ -1,7 +1,16 @@
 package com.dev.marcosfilho.reese;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void login(View view){
+
+        TextView email = findViewById(R.id.email);
+        TextView password = findViewById(R.id.password);
+
+        System.out.print("########################### INICIO ###########################");
+
+
+
     }
 }
